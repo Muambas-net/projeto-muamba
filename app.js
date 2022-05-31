37e107e4fcdacc8454dfa5cd552c9f834df52080
felipetexa <felipe.teixeira91@hotmail.com>
2022-05-31 20:40:36 -0300

    header partial e correção de CSS

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const methodOverride = require('method-override');
+const homeRouter = require('./routes/homeRouter');
 
 const app = express();
 
 app.use(methodOverride('_method'));
 app.use(express.static('public'));
 app.set('view engine', 'ejs'); 
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use(homeRouter);
 
 app.listen(3000, () => {
     console.log('Server online at http://localhost:3000')
