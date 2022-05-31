252be78539a50ac08dff76ebe55e4947b852b0b7
vianagabriel <vianagabrieldacunha@gmail.com>
2022-05-31 19:57:45 -0300

    criando rotas

---
+++ routes/homeRouter.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const homeController = require('../controllers/homeController');
+
+
+router.get('/', homeController.index);
+router.get('/carrinho', homeController.carrinho);
+router.get('/categorias', homeController.categorias);
+router.get('/login', homeController.login);
+router.get('/produtos', homeController.produtos);
+
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
