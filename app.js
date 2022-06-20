fa6409f98dabe801bacd3f7526e8cb80b6ebe38e
felipetexa <felipe.teixeira91@hotmail.com>
2022-06-20 19:25:12 -0300

    consertando conflito com pull do Pablo

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const methodOverride = require('method-override');
 const homeRouter = require('./routes/homeRouter');
-<<<<<<< HEAD
 const userRouter = require('./routes/userRouter');
-=======
 const adminRouter = require('./routes/adminRouter');
->>>>>>> 49d2921b0cc1176fe6a4fcbf8822a6eb88f6f75d
 
 const app = express();
 
@@ -16,11 +13,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(homeRouter);
-<<<<<<< HEAD
 app.use(userRouter);
-=======
 app.use(adminRouter);
->>>>>>> 49d2921b0cc1176fe6a4fcbf8822a6eb88f6f75d
+
 
 app.listen(3000, () => {
     console.log('Server online at http://localhost:3000')
