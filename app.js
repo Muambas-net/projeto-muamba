a4abe8f84cd0bd00134c0cd03a308c4f1d294384
psixp <pablo_six@live.com>
2022-06-20 18:38:01 -0300

    incluindo painel do admin

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const methodOverride = require('method-override');
 const homeRouter = require('./routes/homeRouter');
+const adminRouter = require('./routes/adminRouter');
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(homeRouter);
+app.use(adminRouter);
 
 app.listen(3000, () => {
     console.log('Server online at http://localhost:3000')
---
+++ controllers/adminController.js
@@ -0,0 +1,8 @@
+
+const adminController = {
+    getPainelAdmin: (req, res) => {
+        res.render('admin/painelAdmin');
+    }
+}
+
+module.exports = adminController;
