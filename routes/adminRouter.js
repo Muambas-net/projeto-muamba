351cd01d28f0f2dda00e18bd449d0099d6ae0fe1
psixp <pablo_six@live.com>
2022-06-21 22:10:16 -0300

    adicionando navegação no icone do menu

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/adminController");
 
-router.get("/admin/painelAdmin", adminController.getPainelAdmin);
+router.get("/admin/paineladmin", adminController.getPainelAdmin);
 
 module.exports = router;
