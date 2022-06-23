9d5f8e6fbc770bcea9ddd1251f4796e9b6382b39
felipetexa <felipe.teixeira91@hotmail.com>
2022-06-23 16:34:01 -0300

    adicionando paginas de cadastro, edição e exclusão de produto

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const adminController = require("../controllers/adminController");
 
 router.get("/adm/paineladmin", adminController.getPainelAdmin);
+router.get("/adm/adicionar-produto", adminController.addProduct);
+router.get("/adm/editar-produto", adminController.editProduct);
+router.get("/adm/deletar-produto", adminController.deleteProduct);
 
 module.exports = router;
