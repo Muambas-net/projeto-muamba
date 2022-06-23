481a3ac8134ebd72e0b46ec1cfd99d8c82fa259b
vianagabriel <vianagabrieldacunha@gmail.com>
2022-06-23 13:48:28 -0300

    subindo página de listagem de produtos

--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -9,6 +9,7 @@ router.get('/categorias', homeController.categorias);
 router.get('/login', homeController.login);
 router.get('/produtos', homeController.produtos);
 router.get('/cadastro', homeController.cadastro);
+router.get('/lista', homeController.lista);
 
 
 module.exports = router;
