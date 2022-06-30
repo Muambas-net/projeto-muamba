069b8f5dfbcf8b97abf408700dcf38337d7f10ec
felipetexa <felipe.teixeira91@hotmail.com>
2022-06-30 19:06:20 -0300

    CRUD de adicionar, editar e remover produtos. Inicio da implementação do multer

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,3 +1,4 @@
+const ProdutoModel = require('../models/produtosModel');
 
 const adminController = {
     getPainelAdmin: (req, res) => {
@@ -6,11 +7,50 @@ const adminController = {
     addProduct: (req, res) => {
         res.render('adm/adicionarProduto');
     },
+    storeProduct: (req, res) => {
+        uploadAvatar(req, res, (err) => {
+        const { nome, preco, imagem, ativo, descricao } = req.body;
+        const produto = {
+            nome,
+            preco,
+            imagem: '/images/produtos/' + req.file.filename,
+            ativo: (ativo ? true : false),
+            descricao
+        };
+
+        ProdutoModel.save(produto);
+
+        return res.redirect('/adm/paineladmin');
+    });
+    },
     editProduct: (req, res) => {
-        res.render('adm/editarProduto');
+        const {id} = req.params;
+        const produto = ProdutoModel.findById(id);
+        res.render('adm/editarProduto', {produto});
+    },
+    updateProduct: (req, res) => {
+        const {id} = req.params;
+        const {nome, imagem, preco, ativo, descricao} = req.body;
+        const produto = {
+            id,
+            nome,
+            imagem,
+            preco,
+            ativo: (ativo ? true : false),
+            descricao
+        }
+
+        ProdutoModel.update(id, produto);
+
+        return res.redirect('/adm/paineladmin');
     },
     deleteProduct: (req, res) => {
         res.render('adm/deletarProduto');
+    },
+    destroyProduct: (req, res) => {
+        const {id} = req.params;
+        ProdutoModel.delete(id);
+        return res.redirect('/adm/paineladmin');
     }
 }
 
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -4,7 +4,10 @@ const adminController = require("../controllers/adminController");
 
 router.get("/adm/paineladmin", adminController.getPainelAdmin);
 router.get("/adm/adicionar-produto", adminController.addProduct);
+router.post("/adm/adicionar-produto", adminController.storeProduct);
 router.get("/adm/editar-produto", adminController.editProduct);
+router.put("/adm/editar-produto", adminController.updateProduct);
 router.get("/adm/deletar-produto", adminController.deleteProduct);
+router.delete("/adm/deletar-produto", adminController.destroyProduct);
 
 module.exports = router;
