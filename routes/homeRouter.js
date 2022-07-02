e950b270e23f66579e7445851a9ef29ebf4507e6
psixp <pablo_six@live.com>
2022-07-02 16:02:06 -0300

    atualizando rotas de login e cadastro

--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -6,7 +6,6 @@ const homeController = require('../controllers/homeController');
 router.get('/', homeController.index);
 router.get('/carrinho', homeController.carrinho);
 router.get('/categorias', homeController.categorias);
-router.get('/login', homeController.login);
 router.get('/produtos', homeController.produtos);
 router.get('/cadastro', homeController.cadastro);
 router.get('/lista', homeController.lista);
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,9 +2,13 @@ const express = require("express");
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-
+router.get('/login', userController.mostraLogin);
+router.get('/cadastro', userController.mostraCadastrar);
 router.get('/perfil', userController.logado);
 router.get('/esqueciMinhaSenha', userController.esqueciMinhaSenha);
+
+router.post('/login', userController.login);
+router.post('/cadastro', userController.store);
 
 
 module.exports = router;
