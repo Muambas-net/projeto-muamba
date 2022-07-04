f82d7536f255d3b9d01b8a4831b2985cd4df0086
vianagabriel <vianagabrieldacunha@gmail.com>
2022-07-04 15:04:25 -0300

    implentando session

--- app.js
+++ app.js
@@ -3,14 +3,25 @@ const methodOverride = require('method-override');
 const homeRouter = require('./routes/homeRouter');
 const userRouter = require('./routes/userRouter');
 const adminRouter = require('./routes/adminRouter');
+const session = require('express-session');
 
 const app = express();
+
+
 
 app.use(methodOverride('_method'));
 app.use(express.static('public'));
 app.set('view engine', 'ejs'); 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use(session({
+    secret: 'ecomerce muambas',
+    resave: false,
+    saveUninitialized: true,
+   
+  }))
+
 
 app.use(homeRouter);
 app.use(userRouter);
