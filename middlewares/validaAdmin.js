6003cde04dc6c04ea64f0a2c7bca92936bdac052
psixp <pablo_six@live.com>
2022-07-04 13:10:54 -0300

    incluindo middlewares de validação de admin e login

---
+++ middlewares/validaAdmin.js
@@ -0,0 +1,10 @@
+const validaAdmin = (req, res, next) => {
+    const { usuario } = req.session;
+    if (usuario.nivelId == 'admin') {
+       return next();
+    }
+
+    return res.redirect('/');
+}
+
+module.exports = validaAdmin;
