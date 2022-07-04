ddd43c4646feefda1fd42d41933c1c8f34bd3524
felipetexa <felipe.teixeira91@hotmail.com>
2022-07-04 16:38:52 -0300

    completando campos de adicionar e editar produto

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const userController = require('../controllers/userController');
-
+const validaLogin = require("../middlewares/validaLogin");
 
 router.get('/login', userController.mostraLogin);
-router.get('/cadastrar', userController.mostraCadastrar);
-router.get('/perfil', userController.logado);
-router.get('/esqueciMinhaSenha', userController.esqueciMinhaSenha);
-
 router.post('/login', userController.login);
+router.get('/cadastrar', userController.mostraCadastrar);
 router.post('/cadastrar', userController.store);
+router.get('/esqueciMinhaSenha', userController.esqueciMinhaSenha);
+// router.use(validaLogin);
+router.get('/perfil', userController.logado);
+
 
 
 module.exports = router;
