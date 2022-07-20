ba6837a4420aee6e5d8f2be97360dc93c77453a5
felipetexa <felipe.teixeira91@hotmail.com>
2022-07-19 21:14:20 -0300

    refatorando CSS carrinho dinamico

--- routes/carrinhoRouter.js
+++ routes/carrinhoRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const CarrinhoController = require('../controllers/CarrinhoController');
 const validaLogin = require('../middlewares/validaLogin');
 
-router.use(validaLogin)
+// router.use(validaLogin)
 router.get('/carrinho', CarrinhoController.showCart);
 router.post('/carrinho/adicionar', CarrinhoController.addCart);
 router.delete('/carrinho/:id/remover', CarrinhoController.removeCart);
