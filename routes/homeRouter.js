15e8d2a5874b0a084dfd9a6f3f2f228bda275cc9
felipetexa <felipe.teixeira91@hotmail.com>
2022-07-20 15:47:19 -0300

    ajustando detalhe na página de carrinho

--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const homeController = require('../controllers/homeController');
 
 router.get('/', homeController.showIndex);
-router.get('/produtos/:id', homeController.showOneProduct);
 router.get('/categorias', homeController.categorias);
 router.get('/produtos', homeController.produtos);
 router.get('/cadastro', homeController.cadastro);
 router.get('/lista', homeController.lista);
+router.get('/produtos/:id', homeController.showOneProduct);
 
 
 module.exports = router;
