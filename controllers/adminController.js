3e58fccc9c4006db5736cd748cc43c93f04642d9
psixp <pablo_six@live.com>
2022-08-13 11:16:19 -0300

    update: Atualizações de pag admin

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,4 +1,4 @@
-const {Produto} = require('../models/Produto');
+const { Produto } = require('../models');
 const storage = require('../middlewares/storage');
 const fs = require('fs');
 
@@ -8,46 +8,48 @@ const adminController = {
     getPainelAdmin: async (req, res) => {
         const produtos = await Produto.findAll();
 
-        return res.render('adm/painelAdmin', {produtos: produtos});
+        return res.render('adm/paineladmin', { produtos });
     },
     getProduto: async (req, res) => {
-        const {id} = req.params
+        const { id } = req.params
         const produtos = await Produto.findById(id)
-       
-        return res.render('adm/detalhes', {produtos: produtos})
+
+        return res.render('adm/detalhes', { produtos: produtos })
     },
     addProduct: (req, res) => {
-        return res.render('adm/adicionarProduto' );
+        return res.render('adm/adicionarProduto');
     },
     storeProduct: async (req, res) => {
         uploadImagem(req, res, (err) => {
-        const { nome, preco, desconto, estoque, categoria, ativo, descricao } = req.body;
-        
-        const produto = {
-            nome,
-            preco,
-            desconto,
-            estoque,
-            categoria,
-            imagem: '/images/produtos/' + req.file.filename,
-            ativo: (ativo ? true : false),
-            descricao
-        }
+            const { nome, preco, desconto, estoque, categoria, ativo, descricao } = req.body;
 
-        Produto.create(produto);
-    })
+            const produto = {
+                nome,
+                preco,
+                desconto,
+                estoque,
+                categoria,
+                ativo,
+                descricao,
+                imagem: req.file.filename
+            }
+            Produto.create(produto)
+
+        })
 
         return res.redirect('/adm/paineladmin');
-    
+
     },
+
     editProduct: (req, res) => {
-        const {id} = req.params;
+        const { id } = req.params;
         const produto = Produto.findById(id);
-        return res.render('adm/editarProduto', {produto});
+        return res.render('adm/editarProduto', { produto });
     },
+
     updateProduct: (req, res) => {
-        const {id} = req.params;
-        const {nome, imagem, preco, desconto, estoque, categoria, ativo, descricao} = req.body;
+        const { id } = req.params;
+        const { nome, imagem, preco, desconto, estoque, categoria, ativo, descricao } = req.body;
         const produto = {
             id,
             nome,
@@ -64,19 +66,21 @@ const adminController = {
 
         return res.redirect('/adm/paineladmin');
     },
+
     deleteProduct: (req, res) => {
-        const {id} = req.params;
+        const { id } = req.params;
         const produtos = Produto.findById(id);
-        return res.render('adm/deletarProduto', {produtos: produtos });
+        return res.render('adm/deletarProduto', { produtos: produtos });
     },
+
     destroyProduct: (req, res) => {
-        const {id} = req.params;
+        const { id } = req.params;
         Produto.delete(id);
         try {
             fs.unlinkSync('./public' + produto.imagem)
-          } catch(err) {
+        } catch (err) {
             console.error(err)
-          }
+        }
         return res.redirect('/adm/paineladmin');
     }
 }
